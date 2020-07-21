de5ce0dc5209aa50e9287c3c73cced3b816ebb6f
calpeyser <calpeyser@gmail.com>
2020-07-21 11:23:29 -0400

    share and connect forms

--- src/creator-join/creator-join.component.ts
+++ src/creator-join/creator-join.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-creator-join',
@@ -8,9 +9,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CreatorJoinComponent implements OnInit {
   @Input() visible : boolean;
 
+  joinForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    description: new FormControl(''),
+  })
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    var message = "Submitted, first name: " + this.joinForm.value['firstName']
+
+    alert(message)
+  }
 }
